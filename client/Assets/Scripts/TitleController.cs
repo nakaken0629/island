bf12fb53196635457fb57aa8e58f998dc197821e
NAKAGAKI Kenji <nakaken0629@gmail.com>
2018-03-09 03:08:20 +0900

    タイトルからエントリー画面へ遷移する処理を作製

---
+++ client/Assets/Scripts/TitleController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            SceneManager.LoadScene("Entry");   
+        }
+    }
+}
