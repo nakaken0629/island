2c9cd5637e120cbf37172179a2ead0f2576f5e45
NAKAGAKI Kenji <nakaken0629@gmail.com>
2018-03-24 08:18:20 +0900

    カーソルキーで左右に移動

---
+++ client/Assets/Scripts/EntryController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntryController : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        Debug.Log("start");
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("right");
+            animator.SetInteger("mode", 1);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("left");
+            animator.SetInteger("mode", 0);
+        }
+    }
+}
